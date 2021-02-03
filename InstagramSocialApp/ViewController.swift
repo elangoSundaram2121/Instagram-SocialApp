933846235bc369fc4201efb2b6da0e19780b7729
RajaRajanSara <rajarajankar@gmail.com>
2021-02-03 16:20:44 +0530

    Initial Commit

---
+++ InstagramSocialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramSocialApp
+//
+//  Created by Balachandar S on 03/02/21.
+//  Copyright © 2021 Balachandar S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
